b34fb7836666e7cbc022685b15c577f913403a6f
unknown <fajarcahyadiputra@gmail.com>
2022-10-27 15:55:56 +0700

    rubah

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const {greeting} = require('./user')
 
 app.get('/', (req,res) => res.send('This App is running properly!'))
 app.get('/ping', (req,res) => res.send('Pong!'))
-app.get('/youtube', (req,res) => res.send('Hello, youtube kamu keren seklai okokoko!'))
+app.get('/youtube', (req,res) => res.send('Hello, youtube dimana mana kamu selalau ada!'))
 app.get('/profil/:name', (req,res) => res.send('Hello'+ req.params.name));
 app.get('/hello/:name', (req,res) => {
     res.json({message:greeting(req.params.name)})
